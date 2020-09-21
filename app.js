231eb727673e256c2bbd4b92b2860ee0ace6791a
techdegreeEugene <65640084+techdegreeEugene@users.noreply.github.com>
2020-09-21 15:28:48 -0400

    update app.js

---
+++ app.js
@@ -0,0 +1,70 @@
+
+
+// load modules
+const express = require('express');
+const morgan = require('morgan');
+const usersRoutes = require('./routes/users');
+const coursesRoutes = require('./routes/courses');
+const indexRoutes = require('./routes/index');
+const authRoutes = require('./routes/auth');
+
+
+
+
+// variable to enable global error logging
+const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
+
+// create the Express app
+const app = express();
+app.use(express.json());
+
+// setup morgan which gives us http request logging
+app.use(morgan('dev'));
+
+// TODO setup your api routes here
+
+
+
+// setup a friendly greeting for the root route
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Welcome to the REST API project!',
+  });
+});
+
+app.use('/api/users', usersRoutes);
+app.use('/api/courses', coursesRoutes);
+app.use('/api/index', indexRoutes);
+app.use('/api/auth', authRoutes);
+
+
+// send 404 if no other route matched
+app.use((req, res) => {
+  res.status(404).json({
+    message: 'No Routes Matched',
+  });
+});
+
+// setup a global error handler
+app.use((err, req, res, next) => {
+  if (enableGlobalErrorLogging) {
+    console.error(`Global error handler: ${JSON.stringify(err.stack)}`);
+  }
+
+  res.status(err.status || 500).json({
+    message: err.message,
+    error: {},
+  });
+});
+
+// set our port
+app.set('port', process.env.PORT || 5000);
+
+
+  const server = app.listen(app.get('port'), () => {
+    console.log(`Express server is listening on port ${server.address().port}`);
+});
+
+
+
+
